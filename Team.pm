71f6a9fc3c8abe3642bcefcfa3f0b0a5bb4face2
Andrew Davis <andrew.r.davis@nasa.gov>
2014-01-30 12:54:07 -0500

    Removed getters/setters, etc blah blah

--- Team.pm
+++ Team.pm
@@ -8,63 +8,19 @@ sub new {
 
 		my $class = shift;
 		my $self = {
-				_teamName => shift,
-				_location => shift,
-				_manager => shift,
-				_formation => shift,
+				teamName => shift,
+				location => shift,
+				manager => shift,
+				formation => shift,
+                players => [],
 		};	
-		print "\nTeam: $self->{_teamName}\n";
-		print "Location: $self->{_location}\n";
-		print "Manager: $self->{_manager}\n";
-		print "Formation: $self->{_formation}\n\n";
+		print "\nTeam: $self->{teamName}\n";
+		print "Location: $self->{location}\n";
+		print "Manager: $self->{manager}\n";
+		print "Formation: $self->{formation}\n\n";
 		bless $self, $class;
 		return $self;
 
-}
-
-sub getTeamName {
-		return $self->{_teamName};
-}
-
-sub setTeamName {
-		my ( $self, $teamName ) = @_;
-		$self->{_teamName} = $teamName if defined ($teamName);
-		return $self->{_teamName};
-}
-
-sub getLocation {
-		return $self->{_location};
-}
-
-sub setLocation {
-		my ( $self, $location ) = @_;
-		$self->{_location} = $location if defined ($location);
-		return $self->{_location};
-}
-
-sub getManager {
-		return $self->{_manager};
-}
-
-sub setManager {
-		my ( $self, $manager ) = @_;
-		$self->{_manager} = $manager if defined ($manager);
-		return $self->{_manager};
-}
-
-sub getFormation {
-		return $self->{_formation};
-}
-
-sub setFormation {
-		my ( $self, $formation ) = @_;
-		$self->{_formation} = $formation if defined ($formation);
-		return $self->{_formation};
-}
-
-
-sub buildTeam {
-		@newTeam = require(Player);
 }
 
 1;
--- manchesterUnited.pl
+++ manchesterUnited.pl
@@ -1,30 +1,59 @@
 #!/usr/bin/perl
 
+use strict;
+use warnings;
 use Team;
+use Player;
 
 #Team Info
 
 my $manchesterUnited = new Team ("Manchester United","Manchester, England","David Moyes","4-4-1-1");
 
-$manchesterUnited->buildTeam(
-			@newTeam;
-); 
+
+my $player = Player->new({
+        firstName => 'Andy',
+        lastName => 'Davis',
+        kitNumber => 07,
+        position => 'ST',
+    });
+
+my $player2 = Player->new({
+        fistName => 'Jared',
+        lastName => 'Rodriguez',
+        kitNumber => 22,
+        position => 'CF',
+    });
+
+#$manchesterUnited->{players} = [$player, $player]; 
+
+push($manchesterUnited->{players}, $player);
+push($manchesterUnited->{players}, $player);
+
+print "Sanity Check!\n";
+my @players = $manchesterUnited->{players};
+
+print $#players;
+
+#foreach (@players)
+#{
+#    print $_->{firstName};
+#}
 
 #Forwards
-$striker = new Player ("Robin", "van Persie", 20, "ST");
-$centerFielder = new Player ("Wayne", "Rooney", 10, "CF");
+#$striker = new Player ("Robin", "van Persie", 20, "ST");
+#$centerFielder = new Player ("Wayne", "Rooney", 10, "CF");
 
 #Midfielders
-$leftMid = new Player ("Adnan","Januzaj", 44, "LM");
-$centralMid = new Player ("Darren","Fletcher", 24, "CM");
-$centralMid_Two = new Player ("Michael","Carrick", 16, "CM");
-$rightMid = new Player ("Antonio","Valencia", 25, "RM");
+#$leftMid = new Player ("Adnan","Januzaj", 44, "LM");
+#$centralMid = new Player ("Darren","Fletcher", 24, "CM");
+#$centralMid_Two = new Player ("Michael","Carrick", 16, "CM");
+#$rightMid = new Player ("Antonio","Valencia", 25, "RM");
 
-#Defenders
-$leftBack = new Player ("Rafael","de Silva", 2, "RB");
-$centerBack = new Player ("Nemanja","Vidic", 15, "CB");
-$centerBack_Two = new Player ("Johnny","Evans", 6, "CB");
-$rightBack = new Player ("Patrice","Evra", 3, "LB");
+##Defenders
+#$leftBack = new Player ("Rafael","de Silva", 2, "RB");
+#$centerBack = new Player ("Nemanja","Vidic", 15, "CB");
+#$centerBack_Two = new Player ("Johnny","Evans", 6, "CB");
+#$rightBack = new Player ("Patrice","Evra", 3, "LB");
 
 #Keeper
-$keeper = new Player ("David","de Gea", 1, "GK");
+#$keeper = new Player ("David","de Gea", 1, "GK");
